e9a2a9e708056214fb98c8227d94e8adffa90683
André Martins <andre.martins@koerber.digital>
2022-01-01 20:06:36 +0000

    wip

---
+++ src/views/PaletteView.vue
@@ -0,0 +1,17 @@
+<template>{{ palettes }}</template>
+
+<script>
+import palettes from '@/data/seedPalettes';
+
+export default {
+  name: 'PaletteView',
+
+  data() {
+    return {
+      palettes,
+    };
+  },
+};
+</script>
+
+<style scoped></style>
